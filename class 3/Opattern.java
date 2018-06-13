e5a2b40456c8d34382400f146bccd0ffecf98bc1
SATYAM6316071 <satyamtherockstar007@gmail.com>
2018-06-13 23:40:38 +0530

    init

--- class 3/Opattern.java
+++ class 3/Opattern.java
@@ -2,7 +2,7 @@ class Opattern{
 public static void main(String[] args){
 for (int i=5;i>=0;i--)
 {
-for (int j=1;j<i;j++){
+for (int j=1;j<=i;j++){
 System.out.print("*");
 }
 System.out.println(" ");
