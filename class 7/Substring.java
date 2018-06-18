415d65d19ae5df04c2511a6639dbc61ac40310c4
SATYAM6316071 <satyamtherockstar007@gmail.com>
2018-06-18 23:43:02 +0530

    init

---
+++ class 7/Substring.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Substring
+{
+   public static void main(String[] args)
+     {
+	String str="xyz";
+	System.out.println(str.substring(0,1));
+	System.out.println(str.substring(1,2));
+	System.out.println(str.substring(0,2));
+	System.out.println(str.substring(0));
+	System.out.println(str.substring(1));
+	System.out.println(str.substring(2));
+      }
+}
